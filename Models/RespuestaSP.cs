437c19a43270c6f16db2fcadf6aa6428f61aa50d
Bran <santiagobran34@gmail.com>
2025-10-23 15:45:29 -0500

    Uso de bootstrap y actualizacion de estilos en 3 tablas con foranea

---
+++ Models/RespuestaSP.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FrontendBlazorApi.Models
+{
+    // Clase genérica para mapear la respuesta de la API
+    public class RespuestaSP<T>
+    {
+        [JsonPropertyName("Procedimiento")]
+        public string? Procedimiento { get; set; }
+
+        [JsonPropertyName("Resultados")]
+        public List<Dictionary<string, object>>? Resultados { get; set; }
+
+        [JsonPropertyName("Total")]
+        public int Total { get; set; }
+
+        [JsonPropertyName("Mensaje")]
+        public string? Mensaje { get; set; }
+    }
+}
